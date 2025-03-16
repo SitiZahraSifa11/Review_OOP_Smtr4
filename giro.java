d52e65968819881465c432a08dc26950b1972695
SitiZahraSifa11 <160223744+SitiZahraSifa11@users.noreply.github.com>
2025-03-16 23:25:17 +0700

    Update giro.java

--- giro.java
+++ giro.java
@@ -1,8 +1,8 @@
-public class giro extends rekening {
+public class giro extends Rekening {
     double limitPenarikan;
 
     public giro(String namaPemilik, String nomorRekening, double saldo, double limitPenarikan) {
-        super(namaPemilik, nomorRekening, saldo);
+        super(namaPemilik, nomorRekening, saldo,0);
         this.limitPenarikan = limitPenarikan;
     }
     public double getLimitPenarikan() {
@@ -14,13 +14,30 @@ public class giro extends rekening {
     public void tarik(double jumlah) {
         if (saldo >= jumlah) {
             saldo -= jumlah;
+            System.out.println("===============================================================");
+            System.out.println(" Penarikan berhasil !" );
             System.out.println(namaPemilik + " telah menarik: Rp " + jumlah);
-        } else {
-            System.out.println("Saldo tidak mencukupi untuk penarikan.");
+            System.out.println("===============================================================");
+        }else if((getSaldo()+limitPenarikan)>= jumlah){
+            double sisa = jumlah - getSaldo();
+            setSaldo(0);
+            limitPenarikan-=sisa;
+            System.out.println("===============================================================");
+            System.out.println("Penarikan berhasil dengan menggunakan limit giro.");
+            System.out.println("Sisa limit giro: " + limitPenarikan);
+            System.out.println("===============================================================");
+        } 
+        else {
+
+            System.out.println("===============================================================");
+            System.out.println("Saldo dan limit giro tidak mencukupi untuk penarikan.");
+            System.out.println("===============================================================");
         }
     }
     public void tampilkanInfo() {
         super.tampilkanInfo();
+        System.out.println("===============================================================");
         System.out.println("Limit Penarikan: Rp " + getLimitPenarikan());
+        System.out.println("===============================================================");
     }
 }
