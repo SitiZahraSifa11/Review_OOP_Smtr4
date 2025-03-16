e74cda1d5b3f92b5756ede53ec71978166a72743
SitiZahraSifa11 <160223744+SitiZahraSifa11@users.noreply.github.com>
2025-03-16 23:25:52 +0700

    Update rekening.java

--- rekening.java
+++ rekening.java
@@ -1,11 +1,19 @@
-public class rekening {
+public class Rekening {
     String namaPemilik;
     String nomorRekening;
     double saldo;
-    public rekening(String namaPemilik, String nomorRekening, double saldo) {
+    double limitGiro;
+    public Rekening(String namaPemilik, String nomorRekening, double saldo) {
         this.namaPemilik = namaPemilik;
         this.nomorRekening = nomorRekening;
         this.saldo = saldo;
+        this.limitGiro=0;
+    }
+    public Rekening(String namaPemilik, String nomorRekening, double saldo, double limitGiro) {
+        this.namaPemilik = namaPemilik;
+        this.nomorRekening = nomorRekening;
+        this.saldo = saldo;
+        this.limitGiro=limitGiro;
     }
     public String getNamaPemilik() {
         return namaPemilik;
@@ -25,56 +33,89 @@ public class rekening {
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-
-    public void setor(double jumlah) {
-        saldo += jumlah;
-        System.out.println(namaPemilik + " telah menyetor: Rp " + jumlah);
+    public double getLimitGiro(){
+        return limitGiro;
+    }
+    public void setLimitGiro(double limitGiro){
+        this.limitGiro=limitGiro;
     }
 
+    public void setor(double jumlah) {
+        if(jumlah>0){
+            saldo += jumlah;
+            System.out.println("===============================================================");
+        System.out.println(namaPemilik + " telah menyetor: Rp " + jumlah);
+        System.out.println("Saldo Terkini : Rp " + saldo);
+        System.out.println("===============================================================");
+        }else{
+            System.out.println("===============================================================");
+            System.out.println("Maaf Anda Belum Memasukan Nominal ");
+            System.out.println("===============================================================");
+        }
+    }
+    public void setor(double jumlah, boolean tf){
+        if(jumlah >0){
+            if(tf){
+                double admin=3000;
+                if(jumlah > admin){
+                    saldo += (jumlah-admin);
+                    System.out.println("===============================================================");
+                    System.out.println("Setor dengan metode transfer berhasil!!" );
+                    System.out.println("Biaya admin :  " + admin);
+                    System.out.println("Saldo Terkini : Rp " + saldo);
+
+                    System.out.println("===============================================================");
+                }else{
+                    System.out.println("===============================================================");
+                    System.out.println("Jumlah penyetoran harus lebih besar dari biaya admin!");
+                    System.out.println("===============================================================");
+                }
+            }else{
+                setor(jumlah);
+            }
+        }else{
+            System.out.println("===============================================================");
+            System.out.println("Setoran harus ada nominalnya.");
+            System.out.println("===============================================================");
+        }
+
+    }
     public void tarik(double jumlah) {
         if (saldo >= jumlah) {
             saldo -= jumlah;
+            System.out.println("===============================================================");
+            System.out.println("Penarikan berhasil!");
+            System.out.println("===============================================================");
             System.out.println(namaPemilik + " telah menarik: Rp " + jumlah);
-        } else {
-            System.out.println("Saldo tidak mencukupi untuk penarikan.");
+            System.out.println("Saldo Terkini : Rp " + saldo);
+            System.out.println("===============================================================");
+        } else if((saldo+limitGiro)>=jumlah) {
+            double sisa = jumlah-saldo;
+            saldo=0;
+            limitGiro -= sisa;
+            System.out.println("===============================================================");
+            System.out.println("Penarikan berhasil melalui limit giro.");
+            System.out.println("Sisa limit giro: Rp " + limitGiro);
+            System.out.println("===============================================================");
+            
+        }else{
+            System.out.println("===============================================================");
+            System.out.println("Saldo  dan limit tidak mencukupi untuk penarikan.");
+            System.out.println("===============================================================");
         }
     }
     
     public void tampilkanInfo() {
+        System.out.println("===============================================================");
         System.out.println("Nama Pemilik: " + getNamaPemilik());
         System.out.println("Nomor Rekening: " + getNomorRekening());
         System.out.println("Saldo: Rp " + getSaldo());
+        if (limitGiro > 0){
+            System.out.println("Limit giro: Rp. "+getLimitGiro());
+        }
+        System.out.println("===============================================================");
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// public rekening(String namaPemilik, String nomorRekening, double saldoPertama) {
-//         this.namaPemilik = namaPemilik;
-//         this.nomorRekening = nomorRekening;
-//         this.saldo = saldoPertama;
-//     }
 
 
